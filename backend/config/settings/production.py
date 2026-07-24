9ffb827628853d2e6d7f94b4f6a5559998e98348
Muhammad Taha <tahaghulam10@gmail.com>
2026-07-24 18:57:27 +0500

    chore: enable console logging for production errors

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -15,3 +15,19 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
+    },
+}
